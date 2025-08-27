e43f4b72434e4cdb6e3cd75d371ef1c5a3a98f5b
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-08-27 17:12:37 +0330

    Update eslient.config.js

--- eslient.config.js
+++ eslient.config.js
@@ -10,6 +10,8 @@ export default tseslint.config(
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
+      ecmaVersion: 2020,
+
 
 
 
