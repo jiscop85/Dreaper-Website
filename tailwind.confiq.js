b7cde18f6f94b7300fdc5b8a5afb4f24b148d156
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-02 13:17:53 +0330

    Update tailwind.confiq.js

--- tailwind.confiq.js
+++ tailwind.confiq.js
@@ -115,6 +115,10 @@ export default {
 					from: { height: '0' },
 					to: { height: 'var(--radix-accordion-content-height)' }
 				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' }
+				},
 
 
 
