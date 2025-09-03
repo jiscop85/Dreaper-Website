e583a4b0f0b5b6176f0c72df1a90577ea4c8f5df
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-03 14:01:15 +0330

    Update tailwind.confiq.js

--- tailwind.confiq.js
+++ tailwind.confiq.js
@@ -132,6 +132,9 @@ export default {
 					'0%': { opacity: '0', transform: 'translateY(-30px)' },
 					'100%': { opacity: '1', transform: 'translateY(0)' }
 				},
+				'fade-left': {
+					'0%': { opacity: '0', transform: 'translateX(30px)' },
+					'100%': { opacity: '1', transform: 'translateX(0)' }
 
 
 
