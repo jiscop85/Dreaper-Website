e48f854636cfaa4477b4856846a99b37b2af5e48
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-03 14:05:31 +0330

    Update tailwind.confiq.js

--- tailwind.confiq.js
+++ tailwind.confiq.js
@@ -157,6 +157,8 @@ export default {
 					'0%': { transform: 'translateY(-100%)', opacity: '0' },
 					'100%': { transform: 'translateY(0)', opacity: '1' }
 				},
+				// انیمیشن‌های اسکیل و چرخش
+
 
 
 
