eed39482758f746f0c8dd888e6b897aaf1fdd854
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-03 13:59:28 +0330

    Update tailwind.confiq.js

--- tailwind.confiq.js
+++ tailwind.confiq.js
@@ -122,6 +122,8 @@ export default {
 				// انیمیشن‌های کلاسیک بهبود یافته
 				'fade-in': {
 					'0%': { opacity: '0', transform: 'translateY(20px)' },
+					'100%': { opacity: '1', transform: 'translateY(0)' }
+
 
 
 
