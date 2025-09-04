a232783225908e196c7adaa22c623d33d3e4be26
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-04 11:48:50 +0330

    Update tailwind.confiq.js

--- tailwind.confiq.js
+++ tailwind.confiq.js
@@ -256,6 +256,8 @@ export default {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
 				// انیمیشن‌های ورود
+				'fade-in': 'fade-in 0.6s ease-out',
+
 
 
 
