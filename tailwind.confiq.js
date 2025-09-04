faebe009ac073ff5bbfae3ed2fde271f71c9ffc5
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-04 11:30:01 +0330

    Update tailwind.confiq.js

--- tailwind.confiq.js
+++ tailwind.confiq.js
@@ -219,6 +219,10 @@ export default {
 						animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)'
 				},
 					'50%': {
+						transform: 'translateY(-5px)',
+						animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)'
+					}
+				},
 
 
 
