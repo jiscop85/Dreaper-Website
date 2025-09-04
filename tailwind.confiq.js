c03e6887219c7319d2a9278dcf54e3b7845eca0e
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-04 11:46:43 +0330

    Update tailwind.confiq.js

--- tailwind.confiq.js
+++ tailwind.confiq.js
@@ -248,6 +248,8 @@ export default {
 					'0%, 100%': { backgroundPosition: '0% 0%' },
 					'25%': { backgroundPosition: '100% 0%' },
 					'50%': { backgroundPosition: '100% 100%' },
+					'75%': { backgroundPosition: '0% 100%' }
+
 
 
 
