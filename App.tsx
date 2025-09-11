dd81c1226d85e88b64256baa5a9aec08e55c4567
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-11 14:06:52 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,6 +3,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Orders from "./pages/Orders";
+
 
 
 
