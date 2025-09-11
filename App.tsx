72ca318cdfe18051d8443ced780150a94a173afe
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-11 14:07:05 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,6 +4,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Orders from "./pages/Orders";
+import CategoryPage from "./pages/CategoryPage";
+import Auth from "./pages/Auth";
+
 
 
 
