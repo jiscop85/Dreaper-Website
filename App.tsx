04b89b78646e1914e621224099cee6208ed57095
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-11 14:06:31 +0330

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 
