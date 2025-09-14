0c9e2c27489d3888b11bcf97c42f33dcd1f23443
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-14 10:13:15 +0330

    Update catStore.ts

--- catStore.ts
+++ catStore.ts
@@ -61,3 +61,4 @@ export const useCartStore = create<CartStore>()(
       removeFromCart: (id) => {
         set(state => ({
           items: state.items.filter(item => item.id !== id)
+        }));
