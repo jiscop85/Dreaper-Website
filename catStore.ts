3ab0d608b409d083cefa669e5add2d7bf57de2b0
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-14 10:11:19 +0330

    Update catStore.ts

--- catStore.ts
+++ catStore.ts
@@ -51,4 +51,5 @@ export const useCartStore = create<CartStore>()(
             items: state.items.map(i => 
                  ...item,
               quantity: Math.round(item.quantity * 100) / 100,
+              totalPrice: Math.round(item.totalPrice * 100) / 100
 
