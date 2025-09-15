c1299a293e2bacf656cddc3ca7eb6a13d1c0fe76
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-15 09:52:04 +0330

    Update AllProducts.tsx

--- AllProducts.tsx
+++ AllProducts.tsx
@@ -12,6 +12,8 @@ import Footer from '@/components/Footer';
 
 const AllProducts = () => {
   const { data: products, isLoading } = useProducts();
+  const [searchTerm, setSearchTerm] = useState('');
+
 
 
 
