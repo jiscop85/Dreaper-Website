ee479a3cc816addd671ac701866be711a9565bb4
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-16 20:28:49 +0330

    Update AllProducts.tsx

--- AllProducts.tsx
+++ AllProducts.tsx
@@ -103,6 +103,8 @@ const AllProducts = () => {
             <div className="flex-1 relative">
               <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
               <Input
+                type="text"
+
 
 
 
