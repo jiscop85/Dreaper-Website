a6078e6b0cbea70ffff1d2c2ac7c930b431f3adb
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-16 20:34:04 +0330

    Update AllProducts.tsx

--- AllProducts.tsx
+++ AllProducts.tsx
@@ -136,6 +136,8 @@ const AllProducts = () => {
                 <SelectItem value="price-low">ارزان‌ترین</SelectItem>
                 <SelectItem value="price-high">گران‌ترین</SelectItem>
                 <SelectItem value="discount">بیشترین تخفیف</SelectItem>
+              </SelectContent>
+
 
 
 
