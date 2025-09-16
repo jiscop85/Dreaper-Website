008258a00678f5fd8b98fff624b191279632c017
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-16 20:29:00 +0330

    Update AllProducts.tsx

--- AllProducts.tsx
+++ AllProducts.tsx
@@ -104,6 +104,8 @@ const AllProducts = () => {
               <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
               <Input
                 type="text"
+                placeholder="جستجو در محصولات..."
+
 
 
 
