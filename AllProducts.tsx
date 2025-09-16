bb6118ac3ffa6bd990fcc9e84ff8d5334af0dd8f
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-16 20:30:27 +0330

    Update AllProducts.tsx

--- AllProducts.tsx
+++ AllProducts.tsx
@@ -114,6 +114,8 @@ const AllProducts = () => {
             {/* Category Filter */}
             <Select value={selectedCategory} onValueChange={setSelectedCategory}>
               <SelectTrigger className="w-48 bg-white/50 border-fabric-pearl/50">
+                <SelectValue placeholder="انتخاب دسته‌بندی" />
+
 
 
 
