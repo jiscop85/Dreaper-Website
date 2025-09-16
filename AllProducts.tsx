6392e4a514ef6800f42b14d35fefceccc7ab7a9b
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-16 20:26:50 +0330

    Update AllProducts.tsx

--- AllProducts.tsx
+++ AllProducts.tsx
@@ -90,6 +90,8 @@ const AllProducts = () => {
               </div>
               <div className="bg-white/20 backdrop-blur-sm px-4 py-2 rounded-full">
                 <span className="text-sm">🎨 طرح‌های متنوع</span>
+              </div>
+
 
 
 
