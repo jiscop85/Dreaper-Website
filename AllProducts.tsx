3110b95481366b2c1e6fc8cbf202e7c26e0d2e9d
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-17 10:04:22 +0330

    Update AllProducts.tsx

--- AllProducts.tsx
+++ AllProducts.tsx
@@ -182,6 +182,8 @@ const AllProducts = () => {
             ? 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8' 
             : 'flex flex-col gap-6'
           }
+        `}>
+
 
 
 
