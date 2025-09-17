ac12f7fe002279be5d5e2c200b4126a5d7f00c69
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-17 10:04:00 +0330

    Update AllProducts.tsx

--- AllProducts.tsx
+++ AllProducts.tsx
@@ -180,6 +180,8 @@ const AllProducts = () => {
         <div className={`
           ${viewMode === 'grid' 
             ? 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8' 
+            : 'flex flex-col gap-6'
+
 
 
 
