55b7f73f41a6d5e5074b37c8541f80ae95f7f4dd
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-17 10:09:58 +0330

    Update AllProducts.tsx

--- AllProducts.tsx
+++ AllProducts.tsx
@@ -212,6 +212,8 @@ const AllProducts = () => {
                 className="bg-fabric-navy hover:bg-fabric-terracotta"
               >
                 نمایش همه محصولات
+              </Button>
+
 
 
 
