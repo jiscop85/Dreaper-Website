db5afe2ac640e50f63b2748f170550cb42b212a8
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-18 18:57:23 +0330

    Update Auth.tsx

--- Auth.tsx
+++ Auth.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { ArrowLeft } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
+
 
 
 
