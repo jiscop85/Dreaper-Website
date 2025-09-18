9076dc323c919b0f69da7510a39cf07b3e233d4f
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-19 01:54:04 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -59,6 +59,8 @@ const CategoryPage = () => {
     return (
       <div className="min-h-screen bg-gradient-to-br from-fabric-cream via-white to-fabric-pearl font-vazir">
         <TopBar />
+        <Header />
+
 
 
 
