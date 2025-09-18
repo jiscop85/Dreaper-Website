4e1b8865c032d562dbe32495588cbc9d8467fea9
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-19 01:39:51 +0330

    Update Auth.tsx

--- Auth.tsx
+++ Auth.tsx
@@ -79,6 +79,8 @@ const Auth = () => {
               <div className="mt-6 text-center">
                 <p className="text-gray-600">
                   {isLogin ? 'هنوز حساب کاربری ندارید؟' : 'قبلاً حساب کاربری دارید؟'}
+                </p>
+
 
 
 
