9b0796c1c834523f24b750abdc5a711703a52703
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-18 19:05:25 +0330

    Update Auth.tsx

--- Auth.tsx
+++ Auth.tsx
@@ -60,6 +60,8 @@ const Auth = () => {
                 <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center">
                   <div className="w-4 h-4 bg-gradient-to-br from-fabric-terracotta to-fabric-gold rounded-full"></div>
                 </div>
+              </div>
+
 
 
 
