0f02d078c3efd4a50993b06fac5aa4c8d461bae5
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-18 19:06:00 +0330

    Update Auth.tsx

--- Auth.tsx
+++ Auth.tsx
@@ -64,6 +64,8 @@ const Auth = () => {
               <CardTitle className="text-xl text-fabric-navy">
                 {isLogin ? 'ورود' : 'عضویت'}
               </CardTitle>
+              <CardDescription className="text-gray-600">
+
 
 
 
