d7acf7417713248f58eeb92b621fcd23799ce8ee
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-18 18:59:03 +0330

    Update Auth.tsx

--- Auth.tsx
+++ Auth.tsx
@@ -18,6 +18,8 @@ const Auth = () => {
         <div className="absolute top-1/4 left-1/4 w-64 h-64 bg-fabric-terracotta/5 rounded-full animate-float opacity-60"></div>
         <div className="absolute top-3/4 right-1/4 w-48 h-48 bg-fabric-gold/5 rounded-full animate-float-delayed opacity-60"></div>
         <div className="absolute top-1/2 left-1/2 w-32 h-32 bg-fabric-emerald/5 rounded-full animate-float-slow opacity-60"></div>
+      </div>
+
 
 
 
