62582656f0b9f1d4ac7e7d9b2c07bdf95affdb7d
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-19 01:45:48 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { useFilteredProducts } from '@/hooks/useFilteredProducts';
 import ProductCard from '@/components/ProductCard';
+import TopBar from '@/components/TopBar';
+
 
 
 
