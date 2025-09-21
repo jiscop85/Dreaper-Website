a9e6b75de92b1cfa1d9dafea11b2f38745322cb8
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-21 19:35:18 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -122,6 +122,8 @@ const CategoryPage = () => {
               <select
                 value={sortBy}
                 onChange={(e) => handleSortChange(e.target.value as typeof sortBy)}
+                className="border border-gray-300 rounded-lg px-4 py-2 bg-white text-gray-700 focus:outline-none focus:ring-2 focus:ring-fabric-terracotta"
+
 
 
 
