691e5dad11250c9b53656d480d4d5471e56c281e
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-21 19:22:00 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -94,8 +94,10 @@ const CategoryPage = () => {
                 <button
                   onClick={() => setViewMode('grid')}
                   className={`p-2 rounded transition-colors duration-200 ${
+                    viewMode === 'list' ? 'bg-fabric-terracotta text-white' : 'text-gray-600 hover:bg-gray-100'
 
   
+
 
 
 
