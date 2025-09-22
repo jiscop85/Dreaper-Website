a2b1a705f31a49d19e684b481c7e5473c3daa0b3
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-22 12:59:54 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -211,6 +211,8 @@ const CategoryPage = () => {
             <h2 className="text-2xl font-bold text-gray-700 mb-4">محصولی یافت نشد</h2>
             <p className="text-gray-500 mb-6">
               متأسفانه با فیلترهای انتخابی شما محصولی یافت نشد. لطفاً فیلترهای خود را تغییر دهید.
+            </p>
+
 
 
 
