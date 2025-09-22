6096617d4a5adfff6a8b94fc4dc9b19325f504bd
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-22 12:55:56 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -190,6 +190,8 @@ const CategoryPage = () => {
               ? 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4' 
               : 'grid-cols-1'
           }`}>
+            {products.map((product, index) => (
+
 
 
 
