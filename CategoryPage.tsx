08fd105651df74e20b914f206b27d16d9f8cc3f9
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-22 12:55:24 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -187,6 +187,8 @@ const CategoryPage = () => {
         ) : products && products.length > 0 ? (
           <div className={`grid gap-6 ${
             viewMode === 'grid' 
+              ? 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4' 
+
 
 
 
