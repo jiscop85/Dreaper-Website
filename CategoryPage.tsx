223a2cc97c459f85c298efc50449c3dc0336843e
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-22 12:57:00 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -196,6 +196,8 @@ const CategoryPage = () => {
                 className="animate-fade-in"
                 style={{ animationDelay: `${index * 0.1}s` }}
               >
+                <ProductCard 
+
 
 
 
