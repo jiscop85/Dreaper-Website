386856a9ea3e5fa1e5e4e42b2e34ae94cb6ed9d4
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-22 12:52:58 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -173,6 +173,8 @@ const CategoryPage = () => {
         {/* محتوای اصلی */}
         {isLoading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            {[...Array(8)].map((_, index) => (
+
 
 
 
