d7aef4da4a4c567f39cc657c796030af3335057e
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-22 12:54:06 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -180,6 +180,8 @@ const CategoryPage = () => {
                   <div className="h-4 bg-gray-200 rounded w-3/4"></div>
                   <div className="h-4 bg-gray-200 rounded w-1/2"></div>
                   <div className="h-6 bg-gray-200 rounded w-1/3"></div>
+                </div>
+
 
 
 
