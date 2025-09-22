757d737ac32d2520e510f1cb881145de6350db89
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-22 12:53:08 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -174,6 +174,8 @@ const CategoryPage = () => {
         {isLoading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {[...Array(8)].map((_, index) => (
+              <div key={index} className="bg-white rounded-2xl p-6 animate-pulse">
+
 
 
 
