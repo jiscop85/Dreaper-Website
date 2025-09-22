9c7d32a4e7299f837aabb84f4645f3860b298982
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-22 12:55:34 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -188,6 +188,8 @@ const CategoryPage = () => {
           <div className={`grid gap-6 ${
             viewMode === 'grid' 
               ? 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4' 
+              : 'grid-cols-1'
+
 
 
 
