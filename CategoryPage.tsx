46ff0e9f869196fd38052dc8629f31862b64a713
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-22 12:55:45 +0330

    Update CategoryPage.tsx

--- CategoryPage.tsx
+++ CategoryPage.tsx
@@ -189,6 +189,8 @@ const CategoryPage = () => {
             viewMode === 'grid' 
               ? 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4' 
               : 'grid-cols-1'
+          }`}>
+
 
 
 
