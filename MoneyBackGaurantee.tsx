a37668b407bf0625991e1e404a4f693267f2ec83
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-23 14:18:05 +0330

    Update MoneyBackGaurantee.tsx

--- MoneyBackGaurantee.tsx
+++ MoneyBackGaurantee.tsx
@@ -78,6 +78,8 @@ const MoneyBackGuarantee = () => {
             className="bg-white/20 text-white hover:bg-white/30 backdrop-blur-sm border-white/30"
           >
             بازگشت به فروشگاه
+          </Button>
+
 
 
 
