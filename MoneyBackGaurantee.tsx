308d6979eb08b34b4c6dae46b7c59ea8ee22a3e8
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-23 14:16:20 +0330

    Update MoneyBackGaurantee.tsx

--- MoneyBackGaurantee.tsx
+++ MoneyBackGaurantee.tsx
@@ -66,6 +66,8 @@ const MoneyBackGuarantee = () => {
       {/* Header Section */}
       <div className="relative overflow-hidden bg-gradient-to-r from-orange-500 via-red-500 to-pink-600">
         <div className="absolute inset-0 bg-black/20"></div>
+        <div className="relative container mx-auto px-4 py-20 text-center text-white">
+
 
 
 
