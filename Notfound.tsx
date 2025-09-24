2c2d954c7b709fc90bc62baa382b09e392246f2c
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-24 20:49:06 +0330

    Update Notfound.tsx

--- Notfound.tsx
+++ Notfound.tsx
@@ -14,3 +14,4 @@ const NotFound = () => {
     return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">404</h1>
