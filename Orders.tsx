3f1ecea8d758200ad0107bf5ae5982994b5cff5a
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-25 11:18:11 +0330

    Update Orders.tsx

--- Orders.tsx
+++ Orders.tsx
@@ -7,6 +7,8 @@ import { Textarea } from '@/components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Label } from '@/components/ui/label';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+
 
 
 
