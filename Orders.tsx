4c8bf238de98be273498921be2bb653ab1d0c51c
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-26 12:00:06 +0330

    Update Orders.tsx

--- Orders.tsx
+++ Orders.tsx
@@ -195,6 +195,8 @@ const Orders = () => {
               {/* Order Type */}
               <div>
                 <Label className="text-sm font-medium text-gray-700 mb-3 block">
+                  نوع سفارش *
+
 
 
 
