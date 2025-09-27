6ee1ec202134c79ba6a91b7c011417374463f10a
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-27 11:06:25 +0330

    Update Orders.tsx

--- Orders.tsx
+++ Orders.tsx
@@ -278,6 +278,8 @@ const Orders = () => {
                           }`}>
                             {order.order_type === 'buy' ? 'خرید' : 'فروش'}
                           </span>
+                        </TableCell>
+
 
 
 
