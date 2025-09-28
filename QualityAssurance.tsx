97835ab2f67e89ec1127c9b40aea20734acfc52f
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-28 12:28:56 +0330

    Update QualityAssurance.tsx

--- QualityAssurance.tsx
+++ QualityAssurance.tsx
@@ -67,3 +67,4 @@ const QualityAssurance = () => {
         <div className="mb-20">
           <div className="text-center mb-12">
             <h2 className="text-3xl md:text-4xl font-bold text-fabric-navy mb-4">
+              ویژگی‌های کیفیت ما
