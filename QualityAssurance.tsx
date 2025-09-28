101a608edb7ba731ea9e34f6ea48ecd49b23c060
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-29 01:10:27 +0330

    Update QualityAssurance.tsx

--- QualityAssurance.tsx
+++ QualityAssurance.tsx
@@ -124,4 +124,7 @@ const QualityAssurance = () => {
             ))}
           </div>
         </div>
+        
         {/* Quality Process */}
+        <div className="bg-gradient-to-r from-fabric-navy/5 via-fabric-terracotta/5 to-fabric-gold/5 rounded-3xl p-8 md:p-12">
+
