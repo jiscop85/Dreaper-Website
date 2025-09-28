04785344dd134f4f6c94f105af70c6f61cd11d88
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-29 01:07:21 +0330

    Update QualityAssurance.tsx

--- QualityAssurance.tsx
+++ QualityAssurance.tsx
@@ -111,3 +111,4 @@ const QualityAssurance = () => {
           </div>
          
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            {certificates.map((cert, index) => (
