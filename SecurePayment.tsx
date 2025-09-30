a39212e9cb6359136ea923aa3268aabebddc0636
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-09-30 11:25:56 +0330

    Update SecurePayment.tsx

--- SecurePayment.tsx
+++ SecurePayment.tsx
@@ -97,3 +97,4 @@ const SecurePayment = () => {
         <div className="mb-20">
           <div className="text-center mb-12">
             <h2 className="text-3xl md:text-4xl font-bold text-fabric-navy mb-4">
+              روش‌های پرداخت
