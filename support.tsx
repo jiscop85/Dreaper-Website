79b4191bc5febc5fcfced06e6fe80831c319382e
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-02 11:47:35 +0330

    Update support.tsx

--- support.tsx
+++ support.tsx
@@ -98,3 +98,4 @@ const Support = () => {
       <div className="container mx-auto px-4 pb-16">
         {/* FAQ Section */}
         <div className="mb-20">
+          <div className="text-center mb-12">
