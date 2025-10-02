599e26787260c5c81b1dbc6190efe8dbe513906d
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-02 11:47:55 +0330

    Update support.tsx

--- support.tsx
+++ support.tsx
@@ -100,3 +100,4 @@ const Support = () => {
         <div className="mb-20">
           <div className="text-center mb-12">
             <h2 className="text-3xl md:text-4xl font-bold text-fabric-navy mb-4">
+              سوالات متداول
