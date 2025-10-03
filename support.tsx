3564a54a8c8b562005bae6d0aa265e769850082c
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-03 11:09:36 +0330

    Update support.tsx

--- support.tsx
+++ support.tsx
@@ -166,3 +166,4 @@ const Support = () => {
               <Button className="bg-gradient-to-r from-fabric-terracotta to-fabric-gold text-white px-8 py-3 rounded-xl hover:shadow-lg transition-all duration-300">
                 شروع چت آنلاین
               </Button>
+              <Button
