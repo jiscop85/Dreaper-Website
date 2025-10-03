549ace15924660b97e5d324173eb0381e35a1bdb
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-04 00:48:01 +0330

    Update CustomerSetisfaction.tsx

--- CustomerSetisfaction.tsx
+++ CustomerSetisfaction.tsx
@@ -139,4 +139,5 @@ const CustomerSatisfaction = () => {
            <div className="mb-20">
              <div className="text-center mb-12">
                <h2 className="text-3xl md:text-4xl font-bold text-fabric-navy mb-4">
+                نظرات مشتریان عزیز
 
