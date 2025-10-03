61cfbdc59f2380f21c9ee6d6abd0a6904890dbd4
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-03 11:21:48 +0330

    Update CustomerSetisfaction.tsx

--- CustomerSetisfaction.tsx
+++ CustomerSetisfaction.tsx
@@ -1,1 +1,3 @@
+
 import { Heart, Star, Users, MessageCircle, Award, TrendingUp } from 'lucide-react';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
