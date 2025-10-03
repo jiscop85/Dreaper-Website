f35a2f4c7e80d0546f44c7dd3cfd34f8de0efda2
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-04 00:33:39 +0330

    Update CustomerSetisfaction.tsx

--- CustomerSetisfaction.tsx
+++ CustomerSetisfaction.tsx
@@ -73,3 +73,4 @@ const CustomerSatisfaction = () => {
                   <p className="text-gray-600 font-medium">{stat.label}</p>
                 </CardContent>
               </Card>
+            );
