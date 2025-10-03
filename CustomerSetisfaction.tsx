c1aa302fd931f7fc0b01c13de072069eed26a8eb
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-04 00:51:29 +0330

    Update CustomerSetisfaction.tsx

--- CustomerSetisfaction.tsx
+++ CustomerSetisfaction.tsx
@@ -156,3 +156,4 @@ const CustomerSatisfaction = () => {
                       {[...Array(testimonial.rating)].map((_, i) => (
                         <Star key={i} className="w-4 h-4 fill-yellow-400 text-yellow-400" />
                       ))}
+                    </div>
