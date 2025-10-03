9eef46e8b934a21f1da6c26fdb464ccbddc34301
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-03 11:32:35 +0330

    Update CustomerSetisfaction.tsx

--- CustomerSetisfaction.tsx
+++ CustomerSetisfaction.tsx
@@ -54,3 +54,4 @@ const CustomerSatisfaction = () => {
             className="bg-white/20 text-white hover:bg-white/30 backdrop-blur-sm border-white/30"
           >
             بازگشت به فروشگاه
+          </Button>
