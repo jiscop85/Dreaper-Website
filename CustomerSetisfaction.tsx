53d89c02add98a971c92584df60147fe3721d16c
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-04 00:58:04 +0330

    Update CustomerSetisfaction.tsx

--- CustomerSetisfaction.tsx
+++ CustomerSetisfaction.tsx
@@ -186,4 +186,5 @@ const CustomerSatisfaction = () => {
               onClick={() => navigate('/')}
               className="bg-gradient-to-r from-fabric-terracotta to-fabric-gold text-white px-8 py-3 rounded-xl hover:shadow-lg transition-all duration-300"
             >
+              خرید با اطمینان
 
