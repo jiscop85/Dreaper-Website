caee3fdcd2da518470da2f12bf3b7ae4acd929b8
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-03 11:11:07 +0330

    Update support.tsx

--- support.tsx
+++ support.tsx
@@ -170,3 +170,4 @@ const Support = () => {
                 variant="outline"
                 className="border-fabric-navy text-fabric-navy hover:bg-fabric-navy hover:text-white px-8 py-3 rounded-xl transition-all duration-300"
               >
+                ارسال ایمیل
