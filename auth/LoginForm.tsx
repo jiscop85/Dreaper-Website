18db247362042c4f8700f50ce9fe32aac5b31e3d
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-05 01:59:25 +0330

    Update LoginForm.tsx

--- auth/LoginForm.tsx
+++ auth/LoginForm.tsx
@@ -100,6 +100,8 @@ const LoginForm = () => {
                 onChange={(e) => handleInputChange('phone', e.target.value)}
                 className="pr-10 border-fabric-sand/30 focus:border-fabric-terracotta"
                 required
+              />
+
 
 
 
