57325849ced6031c6c9e165fc9ce4aca425a18c0
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-05 01:43:23 +0330

    Update LoginForm.tsx

--- auth/LoginForm.tsx
+++ auth/LoginForm.tsx
@@ -3,6 +3,8 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Separator } from '@/components/ui/separator';
+
 
 
 
