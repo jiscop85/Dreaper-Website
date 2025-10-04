9e0c33396eaf63e871f0a8b9fe8969a6ad878415
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-05 01:51:05 +0330

    Update LoginForm.tsx

--- auth/LoginForm.tsx
+++ auth/LoginForm.tsx
@@ -62,6 +62,8 @@ const LoginForm = () => {
             value="phone"
             className="data-[state=active]:bg-white data-[state=active]:text-fabric-navy"
           >
+            <Phone className="w-4 h-4 ml-2" />
+
 
 
 
