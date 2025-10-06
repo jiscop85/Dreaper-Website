40fa918f577d797dce897314c4bf8fbad0b7d20c
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-07 01:40:22 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -4,6 +4,8 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Separator } from '@/components/ui/separator';
+import { Checkbox } from '@/components/ui/checkbox';
+
 
 
 
