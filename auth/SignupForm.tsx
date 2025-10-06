5e2293779ac510c49550450682a81795d156217c
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-07 01:45:33 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -44,6 +44,8 @@ const SignupForm = () => {
     let strength = 0;
     if (password.length >= 8) strength += 25;
     if (/[A-Z]/.test(password)) strength += 25;
+    if (/[0-9]/.test(password)) strength += 25;
+
 
 
 
