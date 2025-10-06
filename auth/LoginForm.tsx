9b57ae21011507c668d526c319f8d3f95a9f6047
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-06 23:53:08 +0330

    Update LoginForm.tsx

--- auth/LoginForm.tsx
+++ auth/LoginForm.tsx
@@ -124,6 +124,8 @@ const LoginForm = () => {
           />
           <Lock className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
           <Button
+            type="button"
+
 
 
 
