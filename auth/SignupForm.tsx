61a18ac7387ccf0382774f44f82508abd2b9266b
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-08 01:01:01 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -160,6 +160,8 @@ const SignupForm = () => {
 
       {/* Signup Method Selection */}
       <Tabs value={signupMethod} onValueChange={setSignupMethod} className="w-full">
+        <TabsList className="grid w-full grid-cols-2 bg-fabric-sand/20">
+
 
 
 
