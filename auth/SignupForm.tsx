db70b587e62428ee3edeff0b16ceb930f9bb6470
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-08 00:57:24 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -139,6 +139,8 @@ const SignupForm = () => {
               className="pr-10 border-fabric-sand/30 focus:border-fabric-terracotta"
               required
             />
+            <User className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
+
 
 
 
