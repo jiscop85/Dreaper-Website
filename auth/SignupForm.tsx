0af0e4473080b4d81821d627d14fccae6e73c217
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-08 01:08:16 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -192,6 +192,8 @@ const SignupForm = () => {
                 required
               />
               <Mail className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
+            </div>
+
 
 
 
