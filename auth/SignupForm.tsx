a44022e3e8a0e99d801c9b8978fd7b4c2eb239a6
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-08 00:57:15 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -138,6 +138,8 @@ const SignupForm = () => {
               onChange={(e) => handleInputChange('firstName', e.target.value)}
               className="pr-10 border-fabric-sand/30 focus:border-fabric-terracotta"
               required
+            />
+
 
 
 
