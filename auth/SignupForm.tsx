215010bccb10d50538c7a7dba7c918b1fb121053
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-09 15:04:06 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -333,6 +333,8 @@ const SignupForm = () => {
                   maxLength={6}
                   required
                 />
+                <Shield className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
+
 
 
 
