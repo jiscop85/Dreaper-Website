274e840d933761b257c88778c3fb984956a0e2a1
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-09 15:04:15 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -334,6 +334,8 @@ const SignupForm = () => {
                   required
                 />
                 <Shield className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
+              </div>
+
 
 
 
