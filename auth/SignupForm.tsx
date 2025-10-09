4902961051769e2f1ad962d798c3dc5a4b51baf2
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-09 14:59:41 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -307,6 +307,8 @@ const SignupForm = () => {
               {isLoading ? (
                 <>
                   <Loader2 className="w-4 h-4 animate-spin ml-2" />
+                  در حال ارسال...
+
 
 
 
