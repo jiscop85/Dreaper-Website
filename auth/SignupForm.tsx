7b816c2d4c29abb3f2eea8917b6b3427519b4a0b
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-09 14:44:25 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -218,6 +218,8 @@ const SignupForm = () => {
                 className="absolute left-1 top-1/2 transform -translate-y-1/2 h-8 w-8 p-0 hover:bg-transparent"
                 onClick={() => setShowPassword(!showPassword)}
               >
+                {showPassword ? (
+
 
 
 
