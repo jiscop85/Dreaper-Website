8db5b746b7fc24fd9114be9d47c9d47158afde93
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-09 14:43:26 +0330

    Update SignupForm.tsx

--- auth/SignupForm.tsx
+++ auth/SignupForm.tsx
@@ -212,6 +212,8 @@ const SignupForm = () => {
               />
               <Lock className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
               <Button
+                type="button"
+
 
 
 
