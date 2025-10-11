87f4e16e48614eafecb40d3a81f881cad5667a65
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-11 14:38:47 +0330

    Update accordian.tsx

--- UI/accordian.tsx
+++ UI/accordian.tsx
@@ -9,3 +9,4 @@ const Accordion = AccordionPrimitive.Root
 const AccordionItem = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
