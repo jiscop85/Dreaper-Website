29a5ad30ef9e064cb842928bd047821f0d98c908
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-11 14:43:23 +0330

    Update accordian.tsx

--- UI/accordian.tsx
+++ UI/accordian.tsx
@@ -35,4 +35,5 @@ const AccordionTrigger = React.forwardRef<
       <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
+))
 
