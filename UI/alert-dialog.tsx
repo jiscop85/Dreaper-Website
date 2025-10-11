84d92884a9bb250ac817e284b1f4fcb0bdab67d7
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-11 14:54:08 +0330

    Update alert-dialog.tsx

--- UI/alert-dialog.tsx
+++ UI/alert-dialog.tsx
@@ -2,3 +2,4 @@ import * as React from "react"
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
 import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
