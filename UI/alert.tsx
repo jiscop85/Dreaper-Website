69ec2ac1edb01ea15dd05cc5180f9b6fd94ad9fc
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-12 12:47:31 +0330

    Update alert.tsx

--- UI/alert.tsx
+++ UI/alert.tsx
@@ -22,6 +22,8 @@ const alertVariants = cva(
 const Alert = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof alertVariants>
+>(({ className, variant, ...props }, ref) => (
+
 
 
 
