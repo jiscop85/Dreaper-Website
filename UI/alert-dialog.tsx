9a4e0fd022c90aa62fc1bc042a8d7e609b398b11
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-12 12:35:07 +0330

    Update alert-dialog.tsx

--- UI/alert-dialog.tsx
+++ UI/alert-dialog.tsx
@@ -98,3 +98,4 @@ AlertDialogDescription.displayName =
 const AlertDialogAction = React.forwardRef<
   React.ElementRef<typeof AlertDialogPrimitive.Action>,
   React.ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
+>(({ className, ...props }, ref) => (
