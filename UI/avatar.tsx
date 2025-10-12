83aa598ed2b943e42f1d795eb750dc76f4e3dff4
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-13 00:39:15 +0330

    Update avatar.tsx

--- UI/avatar.tsx
+++ UI/avatar.tsx
@@ -31,6 +31,8 @@ AvatarImage.displayName = AvatarPrimitive.Image.displayName
 const AvatarFallback = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Fallback>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+
 
 
 
