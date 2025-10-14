8e4d5027444a6fdc56ff11da9b0d6c36578fe092
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-15 01:39:44 +0330

    Update button.tsx

--- UI/button.tsx
+++ UI/button.tsx
@@ -12,6 +12,8 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline:
+
 
 
 
