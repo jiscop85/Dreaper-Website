544790831eda070fd98954c9cd68ab7d40b9c629
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-14 12:57:57 +0330

    Update breadcrumb.tsx

--- UI/breadcrumb.tsx
+++ UI/breadcrumb.tsx
@@ -35,7 +35,9 @@ const BreadcrumbItem = React.forwardRef<
     ref={ref}
     className={cn("inline-flex items-center gap-1.5", className)}
     {...props}
-  />
+  />BreadcrumbItem.displayName = "BreadcrumbItem"
+
+
 
 
 
