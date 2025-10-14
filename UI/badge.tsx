79a87d5cdf1d6fb5de193ec3680640c0a2f6528c
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-14 12:33:44 +0330

    Update badge.tsx

--- UI/badge.tsx
+++ UI/badge.tsx
@@ -11,6 +11,8 @@ const badgeVariants = cva(
         default:
           "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
         secondary:
+          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+
 
 
 
