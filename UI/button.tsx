f5130e9e3821f9b15f7053267f5a209731ca183b
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-15 01:44:53 +0330

    Update button.tsx

--- UI/button.tsx
+++ UI/button.tsx
@@ -45,6 +45,8 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+
 
 
 
