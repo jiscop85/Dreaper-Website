a576229ea3e2abfc1fdc980921a7d51ae83aeada
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-15 01:40:05 +0330

    Update button.tsx

--- UI/button.tsx
+++ UI/button.tsx
@@ -15,6 +15,8 @@ const buttonVariants = cva(
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+
 
 
 
