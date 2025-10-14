270bf8f314bbb0765c7382c03a9f9ffab1d2850b
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-15 01:39:51 +0330

    Update button.tsx

--- UI/button.tsx
+++ UI/button.tsx
@@ -13,6 +13,8 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+
 
 
 
