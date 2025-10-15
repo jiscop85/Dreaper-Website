1084c67a60c6c9598b0e50df972796e311d2a5b0
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-16 01:28:21 +0330

    Update Card.tsx

--- UI/Card.tsx
+++ UI/Card.tsx
@@ -58,6 +58,8 @@ CardDescription.displayName = "CardDescription"
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+
 
 
 
