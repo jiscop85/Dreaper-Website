97136945329b5f55dd4786f8b638c120eb07a863
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-16 00:57:16 +0330

    Update Calender.tsx

--- UI/Calender.tsx
+++ UI/Calender.tsx
@@ -41,6 +41,8 @@ function Calendar({
         ),
         day_range_end: "day-range-end",
         day_selected:
+          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+
 
 
 
