aab807bba8e18a65a3c4d3c8542ff32b1cb63253
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-16 00:55:17 +0330

    Update Calender.tsx

--- UI/Calender.tsx
+++ UI/Calender.tsx
@@ -29,6 +29,8 @@ function Calendar({
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
+        table: "w-full border-collapse space-y-1",
+
 
 
 
