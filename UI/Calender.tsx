4f5f3f49e1618b6bbf7c988141c0d4e957e2f3ba
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-16 01:00:32 +0330

    Update Calender.tsx

--- UI/Calender.tsx
+++ UI/Calender.tsx
@@ -55,6 +55,8 @@ function Calendar({
         IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
         IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
       }}
+      {...props}
+
 
 
 
