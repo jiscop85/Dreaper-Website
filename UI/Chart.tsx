dfec2fedd2a9712cb70f8d4b8eb2af71bc75f92a
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-17 03:00:05 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -81,6 +81,8 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
         __html: Object.entries(THEMES)
           .map(
             ([theme, prefix]) => `
+${prefix} [data-chart=${id}] {
+
 
 
 
