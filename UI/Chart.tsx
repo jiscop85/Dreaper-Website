d465c67a46103de22f9c7d3c529d847c943dcf88
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-18 02:15:51 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -147,6 +147,8 @@ const ChartTooltipContent = React.forwardRef<
       if (labelFormatter) {
         return (
           <div className={cn("font-medium", labelClassName)}>
+            {labelFormatter(value, payload)}
+
 
 
 
