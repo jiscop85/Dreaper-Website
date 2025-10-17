2833e507d6f8890d118f1a9ea233b3a95726cb13
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-18 02:24:05 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -202,6 +202,8 @@ const ChartTooltipContent = React.forwardRef<
                   <>
                     {itemConfig?.icon ? (
                       <itemConfig.icon />
+                    ) : (
+
 
 
 
