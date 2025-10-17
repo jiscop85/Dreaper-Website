6c5ba47e508f57cc8b4e6e7313c5167d42b696d5
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-18 02:22:32 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -195,6 +195,8 @@ const ChartTooltipContent = React.forwardRef<
                   "flex w-full flex-wrap items-stretch gap-2 [&>svg]:h-2.5 [&>svg]:w-2.5 [&>svg]:text-muted-foreground",
                   indicator === "dot" && "items-center"
                 )}
+              >
+
 
 
 
