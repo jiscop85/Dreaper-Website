5efab12be3350cf004e322c68f96f18a6c78f183
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-18 02:20:30 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -180,8 +180,9 @@ const ChartTooltipContent = React.forwardRef<
           "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl",
           className
         )}
-
+      >
         {!nestLabel ? tooltipLabel : null}
+
 
 
 
