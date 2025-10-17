260b203d47e6f9ef7940f7ecc6ca8346538f905b
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-18 02:21:19 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -186,6 +186,8 @@ const ChartTooltipContent = React.forwardRef<
           {payload.map((item, index) => {
             const key = `${nameKey || item.name || item.dataKey || "value"}`
             const itemConfig = getPayloadConfigFromPayload(config, item, key)
+            const indicatorColor = color || item.payload.fill || item.color
+
 
 
 
