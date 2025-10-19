040fb6b25880b27b1526a9c104130892d03d8796
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-19 14:35:10 +0330

    Update CheckBox.tsx

--- UI/CheckBox.tsx
+++ UI/CheckBox.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils"
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+
 
 
 
