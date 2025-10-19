ea5ef97c2458a7f444e42782ff12f15fa4134d6b
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-19 14:44:40 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -239,6 +239,8 @@ const ChartTooltipContent = React.forwardRef<
                       {item.value && (
                         <span className="font-mono font-medium tabular-nums text-foreground">
                           {item.value.toLocaleString()}
+                        </span>
+
 
 
 
