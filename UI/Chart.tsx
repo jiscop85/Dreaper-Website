745b79363ce76b82cf4d9e674935b6989a945f0a
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-19 14:42:39 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -227,6 +227,8 @@ const ChartTooltipContent = React.forwardRef<
                     <div
                       className={cn(
                         "flex flex-1 justify-between leading-none",
+                        nestLabel ? "items-end" : "items-center"
+
 
 
 
