11caa868b37d52860d9278c99e2d0a3a9f227626
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-19 14:35:25 +0330

    Update CheckBox.tsx

--- UI/CheckBox.tsx
+++ UI/CheckBox.tsx
@@ -8,6 +8,8 @@ const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 >(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+
 
 
 
