3db8853e1e83df1ce9ccebd409b701de9ea67fb7
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-20 00:36:07 +0330

    Update Carous.tsx

--- UI/Carous.tsx
+++ UI/Carous.tsx
@@ -23,6 +23,8 @@ type CarouselContextProps = {
   carouselRef: ReturnType<typeof useEmblaCarousel>[0]
   api: ReturnType<typeof useEmblaCarousel>[1]
   scrollPrev: () => void
+  scrollNext: () => void
+
 
 
 
