41a94face06f04a6c87b9376721aed27f7e40e77
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-21 12:52:23 +0330

    Update Carous.tsx

--- UI/Carous.tsx
+++ UI/Carous.tsx
@@ -209,6 +209,8 @@ const CarouselPrevious = React.forwardRef<
           ? "-left-12 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
         className
+      )}
+
 
 
 
