6d7a225212a24cf421236912d96d42ff0c4d3f87
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-22 01:40:54 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -290,6 +290,9 @@ const ChartLegendContent = React.forwardRef<
           return (
             <div
               key={item.value}
+              className={cn(
+                "flex items-center gap-1.5 [&>svg]:h-3 [&>svg]:w-3 [&>svg]:text-muted-foreground"
+
 
 
 
