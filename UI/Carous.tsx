3902b4a5f4d8dcd30bb3cdf0fcc6c2553a6d0c39
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-21 12:40:35 +0330

    Update Carous.tsx

--- UI/Carous.tsx
+++ UI/Carous.tsx
@@ -183,6 +183,8 @@ const CarouselItem = React.forwardRef<
       aria-roledescription="slide"
       className={cn(
         "min-w-0 shrink-0 grow-0 basis-full",
+        orientation === "horizontal" ? "pl-4" : "pt-4",
+
 
 
 
