4c9394f7c2264bb5bf261e5a796d22636d50accd
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-22 01:42:33 +0330

    Update Chart.tsx

--- UI/Chart.tsx
+++ UI/Chart.tsx
@@ -300,6 +300,8 @@ const ChartLegendContent = React.forwardRef<
                 <div
                   className="h-2 w-2 shrink-0 rounded-[2px]"
                   style={{
+                    backgroundColor: item.color,
+
 
 
 
