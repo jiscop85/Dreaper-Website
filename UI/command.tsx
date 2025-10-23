5964401528eb920243198dfec75c4bfc5c15c209
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-23 14:40:01 +0330

    Update command.tsx

--- UI/command.tsx
+++ UI/command.tsx
@@ -15,6 +15,8 @@ const Command = React.forwardRef<
     className={cn(
       "flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground",
       className
+    )}
+
 
 
 
