3817237941e2df685c33671d5a3e5cfdbfac4a01
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-24 01:12:51 +0330

    Update context-menu.tsx

--- UI/context-menu.tsx
+++ UI/context-menu.tsx
@@ -95,6 +95,8 @@ const ContextMenuCheckboxItem = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      className
+
 
 
 
