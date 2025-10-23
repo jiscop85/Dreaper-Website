c6a5e2a8f6ec9a90baa6d36540c97975f2fc2f96
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-23 14:55:03 +0330

    Update command.tsx

--- UI/command.tsx
+++ UI/command.tsx
@@ -42,6 +42,8 @@ const CommandInput = React.forwardRef<
   <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
     <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
     <CommandPrimitive.Input
+      ref={ref}
+
 
 
 
