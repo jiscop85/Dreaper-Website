4ff0398ed706599f85331acdb8099dcc788e9d27
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-23 15:01:47 +0330

    Update command.tsx

--- UI/command.tsx
+++ UI/command.tsx
@@ -90,6 +90,8 @@ const CommandGroup = React.forwardRef<
       "overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground",
       className
     )}
+    {...props}
+
 
 
 
