4cf3af64698197f8782a8bc69b2ffa02e2d5cf14
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-24 01:13:53 +0330

    Update context-menu.tsx

--- UI/context-menu.tsx
+++ UI/context-menu.tsx
@@ -102,6 +102,8 @@ const ContextMenuCheckboxItem = React.forwardRef<
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <ContextMenuPrimitive.ItemIndicator>
+        <Check className="h-4 w-4" />
+
 
 
 
