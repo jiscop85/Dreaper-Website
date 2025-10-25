0c3312f1edc375b264d02b9443e124e637fee763
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-25 13:06:22 +0330

    Update context-menu.tsx

--- UI/context-menu.tsx
+++ UI/context-menu.tsx
@@ -125,6 +125,8 @@ const ContextMenuRadioItem = React.forwardRef<
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <ContextMenuPrimitive.ItemIndicator>
+        <Circle className="h-2 w-2 fill-current" />
+
 
 
 
