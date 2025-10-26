c57738ba2bc8567ad10ab315345cc46d746a1ac6
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-26 10:30:45 +0330

    Update dialog.tsx

--- UI/dialog.tsx
+++ UI/dialog.tsx
@@ -27,6 +27,8 @@ DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
 const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+
 
 
 
