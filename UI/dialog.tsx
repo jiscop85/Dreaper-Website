27fbadb1da9f5739ed6bb9f8cf3e9d17881d1a07
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-26 10:41:32 +0330

    Update dialog.tsx

--- UI/dialog.tsx
+++ UI/dialog.tsx
@@ -95,6 +95,8 @@ DialogTitle.displayName = DialogPrimitive.Title.displayName
 const DialogDescription = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Description>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
+>(({ className, ...props }, ref) => (
+
 
 
 
