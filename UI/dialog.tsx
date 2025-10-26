4c6dff33ffb09e222311d46d44f5494ab7da72fc
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-26 10:29:41 +0330

    Update dialog.tsx

--- UI/dialog.tsx
+++ UI/dialog.tsx
@@ -19,6 +19,8 @@ const DialogClose = DialogPrimitive.Close
     className={cn(
       "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
+    )}
+
 
 
 
