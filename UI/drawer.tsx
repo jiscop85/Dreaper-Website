c61583948b33dfa1ab54c3380632ecc5f0ba38c5
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-27 19:59:08 +0330

    Update drawer.tsx

--- UI/drawer.tsx
+++ UI/drawer.tsx
@@ -43,6 +43,8 @@ const DrawerContent = React.forwardRef<
       className={cn(
         "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
         className
+      )}
+
 
 
 
