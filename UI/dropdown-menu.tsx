21641f0293b4c9cf8dfe795796291425bcc216bf
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-28 11:25:34 +0330

    Update dropdown-menu.tsx

--- UI/dropdown-menu.tsx
+++ UI/dropdown-menu.tsx
@@ -97,6 +97,8 @@ const DropdownMenuCheckboxItem = React.forwardRef<
   <DropdownMenuPrimitive.CheckboxItem
     ref={ref}
     className={cn(
+      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+
 
 
 
