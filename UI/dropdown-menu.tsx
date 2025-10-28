4851f434222e22c90a151a55fd0fcf7714ba0edb
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-29 00:04:25 +0330

    Update dropdown-menu.tsx

--- UI/dropdown-menu.tsx
+++ UI/dropdown-menu.tsx
@@ -124,6 +124,8 @@ const DropdownMenuRadioItem = React.forwardRef<
       "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
+    {...props}
+
 
 
 
