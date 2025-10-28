c4a275c125999e83f2a6196f69677b04c21d7870
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-29 00:04:57 +0330

    Update dropdown-menu.tsx

--- UI/dropdown-menu.tsx
+++ UI/dropdown-menu.tsx
@@ -128,6 +128,8 @@ const DropdownMenuRadioItem = React.forwardRef<
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <DropdownMenuPrimitive.ItemIndicator>
+        <Circle className="h-2 w-2 fill-current" />
+
 
 
 
