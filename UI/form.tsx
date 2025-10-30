a805f3b9fba6934a187e78ca43f4c8e2355e86b2
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-31 01:56:06 +0330

    Update form.tsx

--- UI/form.tsx
+++ UI/form.tsx
@@ -79,6 +79,8 @@ const FormItem = React.forwardRef<
   return (
     <FormItemContext.Provider value={{ id }}>
       <div ref={ref} className={cn("space-y-2", className)} {...props} />
+    </FormItemContext.Provider>
+
 
 
 
