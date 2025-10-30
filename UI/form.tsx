4810d06d30751b127c475689e4990de584110c5a
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-31 01:57:39 +0330

    Update form.tsx

--- UI/form.tsx
+++ UI/form.tsx
@@ -88,6 +88,8 @@ const FormLabel = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
 >(({ className, ...props }, ref) => {
+  const { error, formItemId } = useFormField()
+
 
 
 
