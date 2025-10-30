4c41417dabea87029323bb8ac05baca7e8d6d4e5
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-31 01:57:28 +0330

    Update form.tsx

--- UI/form.tsx
+++ UI/form.tsx
@@ -87,6 +87,8 @@ FormItem.displayName = "FormItem"
 const FormLabel = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+>(({ className, ...props }, ref) => {
+
 
 
 
