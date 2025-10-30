24b61a0f3c7c11a61cca9b63194bb7647f9c2052
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-31 01:50:02 +0330

    Update form.tsx

--- UI/form.tsx
+++ UI/form.tsx
@@ -44,6 +44,8 @@ const useFormField = () => {
   const itemContext = React.useContext(FormItemContext)
   const { getFieldState, formState } = useFormContext()
 
+  const fieldState = getFieldState(fieldContext.name, formState)
+
 
 
 
