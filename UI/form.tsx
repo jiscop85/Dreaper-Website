047f8e041e91a58e6fd5bf48f0e77b742cfabb74
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-10-31 02:12:01 +0330

    Update form.tsx

--- UI/form.tsx
+++ UI/form.tsx
@@ -145,6 +145,8 @@ const FormMessage = React.forwardRef<
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, children, ...props }, ref) => {
   const { error, formMessageId } = useFormField()
+  const body = error ? String(error?.message) : children
+
 
 
 
