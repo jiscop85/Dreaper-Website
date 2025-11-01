b1bd8268acff856ea946ff69e1d36821b51b40ba
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-01 13:31:20 +0330

    Update input.tsx

--- UI/input.tsx
+++ UI/input.tsx
@@ -60,6 +60,8 @@ const InputOTPSeparator = React.forwardRef<
   React.ElementRef<"div">,
   React.ComponentPropsWithoutRef<"div">
 >(({ ...props }, ref) => (
+  <div ref={ref} role="separator" {...props}>
+
 
 
 
