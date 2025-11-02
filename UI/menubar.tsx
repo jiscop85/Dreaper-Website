34b237a9ac2d4223fcb609313e96d6523f9a53e0
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-03 00:25:27 +0330

    Update menubar.tsx

--- UI/menubar.tsx
+++ UI/menubar.tsx
@@ -137,6 +137,8 @@ const MenubarCheckboxItem = React.forwardRef<
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+      <MenubarPrimitive.ItemIndicator>
+
 
 
 
