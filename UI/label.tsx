fbc52ca4778618ae94e1a0b9bf6dea82508c5a3d
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-02 21:48:53 +0330

    Update label.tsx

--- UI/label.tsx
+++ UI/label.tsx
@@ -11,6 +11,8 @@ const labelVariants = cva(
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+    VariantProps<typeof labelVariants>
+
 
 
 
