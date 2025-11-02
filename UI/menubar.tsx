9aa486425b9bee92e3365066eef90ee97a65f230
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-03 00:14:29 +0330

    Update menubar.tsx

--- UI/menubar.tsx
+++ UI/menubar.tsx
@@ -83,6 +83,8 @@ MenubarSubContent.displayName = MenubarPrimitive.SubContent.displayName
 const MenubarItem = React.forwardRef<
   React.ElementRef<typeof MenubarPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof MenubarPrimitive.Item> & {
+    inset?: boolean
+
 
 
 
