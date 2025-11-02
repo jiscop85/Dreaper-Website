7ec6017ceaf2735fa959af1c316e01c5fdc85df9
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-03 00:30:27 +0330

    Update menubar.tsx

--- UI/menubar.tsx
+++ UI/menubar.tsx
@@ -159,6 +159,8 @@ const MenubarRadioItem = React.forwardRef<
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+      <MenubarPrimitive.ItemIndicator>
+
 
 
 
