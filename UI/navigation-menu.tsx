e73c4df0713ae26a3e26984b0c8c32321b755d8a
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-04 01:07:12 +0330

    Update navigation-menu.tsx

--- UI/navigation-menu.tsx
+++ UI/navigation-menu.tsx
@@ -58,6 +58,8 @@ const NavigationMenuTrigger = React.forwardRef<
       className="relative top-[1px] ml-1 h-3 w-3 transition duration-200 group-data-[state=open]:rotate-180"
       aria-hidden="true"
     />
+  </NavigationMenuPrimitive.Trigger>
+
 
 
 
