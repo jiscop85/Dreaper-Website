dcb5e75bf2f66fbc6efe099a42bb5596549daa9f
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-04 00:56:31 +0330

    Update navigation-menu.tsx

--- UI/navigation-menu.tsx
+++ UI/navigation-menu.tsx
@@ -6,6 +6,8 @@ import { ChevronDown } from "lucide-react"
 import { cn } from "@/lib/utils"
 
 const NavigationMenu = React.forwardRef<
+  React.ElementRef<typeof NavigationMenuPrimitive.Root>,
+
 
 
 
