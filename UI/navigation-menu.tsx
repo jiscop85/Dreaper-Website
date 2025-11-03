19ed492588fecf6145c0d8f5cce22303bdfa9000
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-04 01:08:20 +0330

    Update navigation-menu.tsx

--- UI/navigation-menu.tsx
+++ UI/navigation-menu.tsx
@@ -65,6 +65,8 @@ NavigationMenuTrigger.displayName = NavigationMenuPrimitive.Trigger.displayName
 const NavigationMenuContent = React.forwardRef<
   React.ElementRef<typeof NavigationMenuPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof NavigationMenuPrimitive.Content>
+>(({ className, ...props }, ref) => (
+
 
 
 
