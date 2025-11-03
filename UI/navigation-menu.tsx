29e48e66c02b8b3f076fcdd2131e45678737265c
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-04 01:00:01 +0330

    Update navigation-menu.tsx

--- UI/navigation-menu.tsx
+++ UI/navigation-menu.tsx
@@ -30,6 +30,8 @@ const NavigationMenuList = React.forwardRef<
   <NavigationMenuPrimitive.List
     ref={ref}
     className={cn(
+      "group flex flex-1 list-none items-center justify-center space-x-1",
+
 
 
 
