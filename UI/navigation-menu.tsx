c7dad9954843bf8b03d731d7842eb23b77a627b5
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-04 01:33:49 +0330

    Update navigation-menu.tsx

--- UI/navigation-menu.tsx
+++ UI/navigation-menu.tsx
@@ -109,6 +109,8 @@ const NavigationMenuIndicator = React.forwardRef<
     )}
     {...props}
   >
+    <div className="relative top-[60%] h-2 w-2 rotate-45 rounded-tl-sm bg-border shadow-md" />
+
 
 
 
