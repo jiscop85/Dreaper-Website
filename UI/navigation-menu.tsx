c2697d8cec3afea7244851a85e6742e369f8da45
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-04 01:33:26 +0330

    Update navigation-menu.tsx

--- UI/navigation-menu.tsx
+++ UI/navigation-menu.tsx
@@ -106,6 +106,8 @@ const NavigationMenuIndicator = React.forwardRef<
     className={cn(
       "top-full z-[1] flex h-1.5 items-end justify-center overflow-hidden data-[state=visible]:animate-in data-[state=hidden]:animate-out data-[state=hidden]:fade-out data-[state=visible]:fade-in",
       className
+    )}
+
 
 
 
