97cbb34ae9c13e639b3c0de2f3923c763f451ad1
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-05 15:07:51 +0330

    Update pagination.tsx

--- UI/pagination.tsx
+++ UI/pagination.tsx
@@ -10,6 +10,8 @@ const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
     aria-label="pagination"
     className={cn("mx-auto flex w-full justify-center", className)}
     {...props}
+  />
+
 
 
 
