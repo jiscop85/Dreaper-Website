23257794a4329d6fa19840e008ec3b8e6ca55fcd
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-06 02:33:13 +0330

    Update popover.tsx

--- UI/popover.tsx
+++ UI/popover.tsx
@@ -11,6 +11,8 @@ const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+  <PopoverPrimitive.Portal>
+
 
 
 
