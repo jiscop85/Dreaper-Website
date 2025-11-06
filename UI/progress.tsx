33e53246525253734ebf3520fa8557d3426b44f2
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-07 00:12:25 +0330

    Update progress.tsx

--- UI/progress.tsx
+++ UI/progress.tsx
@@ -5,5 +5,7 @@ import { cn } from "@/lib/utils"
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+
 
 
