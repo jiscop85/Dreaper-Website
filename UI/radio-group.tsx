5165343e5e974e93c1d3366621e769b997541afa
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-07 00:05:39 +0330

    Update radio-group.tsx

--- UI/radio-group.tsx
+++ UI/radio-group.tsx
@@ -19,6 +19,8 @@ const RadioGroup = React.forwardRef<
 RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 
 const RadioGroupItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+
 
 
 
