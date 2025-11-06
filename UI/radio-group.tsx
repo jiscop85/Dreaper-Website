fd749b5c72f42396ae6e70ea9fc57d88f5c8cabc
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-07 00:05:58 +0330

    Update radio-group.tsx

--- UI/radio-group.tsx
+++ UI/radio-group.tsx
@@ -21,6 +21,8 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 const RadioGroupItem = React.forwardRef<
   React.ElementRef<typeof RadioGroupPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, ...props }, ref) => {
+
 
 
 
