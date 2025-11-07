270013f363af3832add9f28f86985a9a59a835d7
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-08 00:36:51 +0330

    Update select.tsx

--- UI/select.tsx
+++ UI/select.tsx
@@ -144,3 +144,15 @@ const SelectSeparator = React.forwardRef<
 ))
 SelectSeparator.displayName = SelectPrimitive.Separator.displayName
 
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+}
