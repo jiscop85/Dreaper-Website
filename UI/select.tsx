503e7fb868e527213e4eca010fb48c7ad7f937f8
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-08 00:31:58 +0330

    Update select.tsx

--- UI/select.tsx
+++ UI/select.tsx
@@ -81,3 +81,8 @@ const SelectContent = React.forwardRef<
       position={position}
       {...props}
     >
+  <SelectScrollUpButton />
+      <SelectPrimitive.Viewport
+        className={cn(
+          "p-1",
+          position === "popper" &&
