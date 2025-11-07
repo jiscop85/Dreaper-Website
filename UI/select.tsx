8985d77b2df644eb94e35a5965ad3a11461325ab
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-08 00:27:47 +0330

    Update select.tsx

--- UI/select.tsx
+++ UI/select.tsx
@@ -7,3 +7,9 @@ import { cn } from "@/lib/utils"
 const Select = SelectPrimitive.Root
 
 const SelectGroup = SelectPrimitive.Group
+
+const SelectValue = SelectPrimitive.Value
+
+const SelectTrigger = React.forwardRef<
+  React.ElementRef<typeof SelectPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
