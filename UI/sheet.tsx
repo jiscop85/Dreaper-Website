28b652863f686c09e8aae264a24ad19d501fcc70
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-10 23:35:51 +0330

    Update sheet.tsx

--- UI/sheet.tsx
+++ UI/sheet.tsx
@@ -99,3 +99,14 @@ const SheetFooter = ({
   />
 )
 SheetFooter.displayName = "SheetFooter"
+
+const SheetTitle = React.forwardRef<
+  React.ElementRef<typeof SheetPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <SheetPrimitive.Title
+    ref={ref}
+    className={cn("text-lg font-semibold text-foreground", className)}
+    {...props}
+  />
+))
