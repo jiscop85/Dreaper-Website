05304fe6acd8c987fc99ed379b90a18a7b0e73a6
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-11 00:47:17 +0330

    Update table.tsx

--- UI/table.tsx
+++ UI/table.tsx
@@ -35,3 +35,18 @@ const TableBody = React.forwardRef<
   />
 ))
 TableBody.displayName = "TableBody"
+
+const TableFooter = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => (
+  <tfoot
+    ref={ref}
+    className={cn(
+      "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0",
+      className
+    )}
+    {...props}
+  />
+))
+TableFooter.displayName = "TableFooter"
