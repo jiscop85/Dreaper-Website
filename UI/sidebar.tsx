7d524fbbbfb457c6bd618235c23441a91345fbf0
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-10 23:38:17 +0330

    Update sidebar.tsx

--- UI/sidebar.tsx
+++ UI/sidebar.tsx
@@ -33,3 +33,14 @@ type SidebarContext = {
   isMobile: boolean
   toggleSidebar: () => void
 }
+
+const SidebarContext = React.createContext<SidebarContext | null>(null)
+
+function useSidebar() {
+  const context = React.useContext(SidebarContext)
+  if (!context) {
+    throw new Error("useSidebar must be used within a SidebarProvider.")
+  }
+
+  return context
+}
