d431b1320ba3b5cad000f9ffab5a8e0a58adc72d
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-10 23:35:39 +0330

    Update sheet.tsx

--- UI/sheet.tsx
+++ UI/sheet.tsx
@@ -85,3 +85,17 @@ const SheetHeader = ({
   />
 )
 SheetHeader.displayName = "SheetHeader"
+
+const SheetFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      className
+    )}
+    {...props}
+  />
+)
+SheetFooter.displayName = "SheetFooter"
