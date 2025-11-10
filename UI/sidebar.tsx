3e05b6a2e4620c2da59a639aa67b507a912b929e
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-10 23:41:44 +0330

    Update sidebar.tsx

--- UI/sidebar.tsx
+++ UI/sidebar.tsx
@@ -362,3 +362,18 @@ const SidebarHeader = React.forwardRef<
   )
 })
 SidebarHeader.displayName = "SidebarHeader"
+
+const SidebarFooter = React.forwardRef<
+  HTMLDivElement,
+  React.ComponentProps<"div">
+>(({ className, ...props }, ref) => {
+  return (
+    <div
+      ref={ref}
+      data-sidebar="footer"
+      className={cn("flex flex-col gap-2 p-2", className)}
+      {...props}
+    />
+  )
+})
+SidebarFooter.displayName = "SidebarFooter"
