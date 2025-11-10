2653d4a74fb31f847ee30f94e938fa29f9b07849
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-10 23:34:11 +0330

    Update sheet.tsx

--- UI/sheet.tsx
+++ UI/sheet.tsx
@@ -4,3 +4,13 @@ import { X } from "lucide-react"
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+
+const Sheet = SheetPrimitive.Root
+
+const SheetTrigger = SheetPrimitive.Trigger
+
+const SheetClose = SheetPrimitive.Close
+
+const SheetPortal = SheetPrimitive.Portal
+
+const SheetOverlay = React.forwardRef<
