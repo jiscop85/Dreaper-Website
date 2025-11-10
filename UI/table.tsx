3621d9b022cddd161db28d7325753dbb73800f72
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-11 00:47:43 +0330

    Update table.tsx

--- UI/table.tsx
+++ UI/table.tsx
@@ -65,3 +65,18 @@ const TableRow = React.forwardRef<
   />
 ))
 TableRow.displayName = "TableRow"
+
+const TableHead = React.forwardRef<
+  HTMLTableCellElement,
+  React.ThHTMLAttributes<HTMLTableCellElement>
+>(({ className, ...props }, ref) => (
+  <th
+    ref={ref}
+    className={cn(
+      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      className
+    )}
+    {...props}
+  />
+))
+TableHead.displayName = "TableHead"
