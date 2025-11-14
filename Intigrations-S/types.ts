2dd64a4e882a89e3d7169819e6f5f23861658a0f
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-15 01:22:36 +0330

    Update types.ts

--- Intigrations-S/types.ts
+++ Intigrations-S/types.ts
@@ -76,3 +76,15 @@ Insert: {
           sort_order?: number | null
           updated_at?: string | null
         }
+  Update: {
+          created_at?: string | null
+          description?: string | null
+          id?: string
+          image_url?: string | null
+          is_active?: boolean | null
+          name?: string
+          parent_id?: string | null
+          slug?: string
+          sort_order?: number | null
+          updated_at?: string | null
+        }
