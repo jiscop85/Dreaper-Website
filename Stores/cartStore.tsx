07c310b8992a16063fcac823f78eeec7c5fe6d28
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-15 01:37:32 +0330

    Update cartStore.tsx

---
+++ Stores/cartStore.tsx
@@ -0,0 +1,12 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface CartItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+  unit: string;
+  image: string;
+  totalPrice: number;
+}
