98ae75e6385da359f926aa8804580d30bd5fc752
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-15 01:37:47 +0330

    Update cartStore.tsx

--- Stores/cartStore.tsx
+++ Stores/cartStore.tsx
@@ -10,3 +10,14 @@ interface CartItem {
   image: string;
   totalPrice: number;
 }
+
+interface CartStore {
+  items: CartItem[];
+  totalAmount: number;
+  totalItems: number;
+  addToCart: (item: CartItem) => void;
+  removeFromCart: (id: string) => void;
+  updateQuantity: (id: string, quantity: number) => void;
+  clearCart: () => void;
+  calculateTotals: () => void;
+}
