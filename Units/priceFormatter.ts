a6854a63a9de45a061fc930f1b7b9679c0315571
Jisc_op <127202191+jiscop85@users.noreply.github.com>
2025-11-17 00:36:19 +0330

    Update priceFormatter.ts

---
+++ Units/priceFormatter.ts
@@ -0,0 +1,9 @@
+
+export const formatPrice = (price: number): string => {
+  return new Intl.NumberFormat('fa-IR').format(price) + ' تومان';
+};
+
+export const calculateDiscountPrice = (price: number, discountPercentage?: number): number => {
+  if (!discountPercentage) return price;
+  return price - (price * discountPercentage / 100);
+};
